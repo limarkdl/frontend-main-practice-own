16dae5e1be24998141844c38fe0ab8b27da6200c
limarkdl <IKostin@uclan.ac.uk>
2023-06-21 10:57:21 +0300

    Updated production build

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -6,15 +6,15 @@ const AboutPage = () => {
     return (
         <div style={{ display: 'flex', gap: '10px', flexDirection: 'column' }}>
             <h1>{t('AboutPage')}</h1>
+            <p>{t('AboutPageDescription')}</p>
             <a
-                style={{ fontSize: '32px' }}
                 target="_blank"
                 href="https://github.com/limarkdl/frontend-main-practice-own"
                 rel="noreferrer"
             >
+                {/* eslint-disable-next-line i18next/no-literal-string */}
                 https://github.com/limarkdl/frontend-main-practice-own
             </a>
-            <p>{t('AboutPageDescription')}</p>
         </div>
     );
 };
