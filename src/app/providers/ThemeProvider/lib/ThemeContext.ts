694523dbb2aab63a975214905da44d9f960294ff
Ivan Kostin <IKostin@uclan.ac.uk>
2023-07-05 18:36:40 +0300

    Fix theme storage

--- src/app/providers/ThemeProvider/lib/ThemeContext.ts
+++ src/app/providers/ThemeProvider/lib/ThemeContext.ts
@@ -12,4 +12,4 @@ export interface ThemeContextProps {
 }
 export const ThemeContext = createContext<ThemeContextProps>({});
 
-export const LOCAL_STORAGE_THEME_KEY = 'theme';
+export const LOCAL_STORAGE_THEME_KEY = 'MyTheme';
