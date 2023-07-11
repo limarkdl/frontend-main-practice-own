af46220d7cc754cba3c3c67a652094fff3c76133
limarkdl <ikostin@uclan.ac.uk>
2023-07-11 23:22:30 +0300

    Trying to configure Azure with additional packages/dir :'(

--- src/shared/config/i18n/i18n.ts
+++ src/shared/config/i18n/i18n.ts
@@ -17,7 +17,7 @@ i18n
             escapeValue: false,
         },
         backend: {
-            loadPath: './locales/{{lng}}/{{ns}}.json',
+            loadPath: 'https://limarkdluclanshopstatic.blob.core.windows.net/public/locales/{{lng}}/{{ns}}.json',
         },
     });
 export default i18n;
